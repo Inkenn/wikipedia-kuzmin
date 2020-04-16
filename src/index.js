4910586a53e6a790b53becaf1bc36e37e25b458d
Lev <liokuza@gmail.com>
2020-04-16 18:24:54 +0300

    Создание первой рабочей версии

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import WikiApp from './wikiapp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WikiApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/wikiapp.js
@@ -0,0 +1,80 @@
+import React from 'react';
+class WikiApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rp: 5,
+            pt: [],
+            renderdone: false,
+        };
+    };
+    async createRandomPages() {
+        var pageurls = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=random&rnlimit=' + this.state.rp;
+        var response = await fetch(pageurls);
+        var trashpages = await response.json();
+        var pages = trashpages.query.random;
+        var pagestitles = [];
+        for(let i = 0; i < pages.length; i++)
+        {
+            pagestitles[i] = pages[i].title;
+        }
+        this.setState(
+            {
+                pt: pagestitles,
+                renderdone: true}
+        );
+    }
+//    handleRefresh(newrp)
+//    {
+//        this.setState ({
+//            rp: newrp,
+//        });
+//    }
+    handleRefresh()
+    {
+        let emptypages = [];
+        this.setState( {
+            pt: emptypages,
+            renderdone: false,
+            rp: 5,
+        });
+    }
+    render() {
+        if(!this.state.renderdone) this.createRandomPages();
+        const pages = this.state.pt;
+        const list = pages.map((title, pindex) => 
+        {
+            let href = 'https://wikipedia.org/wiki/' + title;
+            return(
+            <div className="row" key={pindex}>
+                <div className="col-md-12">
+                    <h3>
+                    {title}
+                    </h3>
+                    <p>
+                      <a className="btn" href={href}>На страницу »</a>
+                    </p>
+                </div>
+            </div>
+            );
+        });
+        return(
+<div className="container-fluid">
+	<div className="row">
+		<div className="col-md-12">
+			<h2 className="text-center">
+				Генерируем случайные страницы!
+			</h2>
+            {list}
+			<div className="row">
+				<div className="col-md-12">
+                    <button type="button" className="btn btn-secondary" onClick={() => this.handleRefresh()}>Сгенерировать ещё!</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+        );
+    }
+}
+export default WikiApp; 
