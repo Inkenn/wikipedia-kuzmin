cac33a39761aa7ff66ed4493431ee0a3330fa1b9
Lev <liokuza@gmail.com>
2020-04-17 14:43:46 +0300

    Работа с артефактами

---
+++ dist/precache-manifest.63e38a2acdf06d21d2f1a7127613a305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e4717647b0e547065c",
+    "url": "0.bundle.js"
+  },
+  {
+    "revision": "e95a017bc12d6e6a6bf6",
+    "url": "2.bundle.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "2.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d623e962de80b957a49",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "5ebf31b3858fd2697d01087937521a54",
+    "url": "index.html"
+  },
+  {
+    "revision": "65e4717647b0e547065c",
+    "url": "static/css/main.5ecd60fb.chunk.css"
+  }
+]);
---
+++ dist/precache-manifest.ce642c5764b058f1d6dac17376e45288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e492f896f3241535c2414ff0d8f07b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f1d7672469253ebaf00",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3aab97be0840740f34ea",
+    "url": "/static/js/2.d00da34f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d00da34f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1d7672469253ebaf00",
+    "url": "/static/js/main.8025c4f2.chunk.js"
+  },
+  {
+    "revision": "04fac001ff80ec17aead",
+    "url": "/static/js/runtime-main.2db6c2ba.js"
+  }
+]);
