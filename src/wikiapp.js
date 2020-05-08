565cf04bbbe667b89f77a94adf6f70718915eae1
Lev Kuzmin <liokuza@gmail.com>
2020-05-09 00:12:29 +0300

    Исправления лаб 2,3

--- src/wikiapp.js
+++ src/wikiapp.js
@@ -1,4 +1,11 @@
 import React from 'react';
+function titler(pages) {
+    let pagestitles = [];
+    for (let i = 0; i < pages.length; i++) {
+        pagestitles[i] = pages[i].title;
+    }
+    return pagestitles;
+}
 class WikiApp extends React.Component {
     constructor(props) {
         super(props);
@@ -6,74 +13,72 @@ class WikiApp extends React.Component {
             rp: 5,
             pt: [],
         };
-        this.createRandomPages();
     };
-    async createRandomPages() {
-        var pageurls = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=random&rnlimit=' + this.state.rp;
+    async getFetch() {
+        var pageurls = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=random&rnlimit=5';
         var response = await fetch(pageurls);
+        return response;
+    }
+    async createRandomPages() {
+        var response = await this.getFetch();
         var trashpages = await response.json();
-        var pages = trashpages.query.random;
-        var pagestitles = [];
-        for(let i = 0; i < pages.length; i++)
-        {
-            pagestitles[i] = pages[i].title;
-        }
-        this.setState((prevState) => 
-        ({
-                pt: pagestitles,
+        this.setState((prevState) =>
+            ({
+                pt: titler(trashpages.query.random),
             })
         );
         return true;
     }
-    handleRefresh()
-    {
+    handleRefresh() {
         let emptypages = [];
-        this.setState((prevState) =>  ({
+        this.setState((prevState) => ({
             pt: emptypages,
             rp: 5,
         }));
         this.createRandomPages();
     }
-    shouldComponentUpdate(nextProps, nextState)
-    {
-        if(this.state.pt.length) return false;
+    componentDidMount() {
+        this.createRandomPages();
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.pt.length) return false;
         return true;
     }
     render() {
         const pages = this.state.pt;
-        const list = pages.map((title, pindex) => 
-        {
+        const list = pages.map((title, pindex) => {
             let href = 'https://wikipedia.org/wiki/' + title;
-            return(
-            <div className="row" key={pindex}>
-                <div className="col-md-12">
-                    <h3>
-                    {title}
-                    </h3>
-                    <p>
-                      <a className="btn" href={href}>На страницу »</a>
-                    </p>
+            return (
+                <div className="row" key={pindex}>
+                    <div className="col-md-12">
+                        <h3 id={'h_' + pindex}>
+                            {title}
+                        </h3>
+                        <p>
+                            <a className="btn" href={href}>На страницу »</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
             );
         });
-        return(
-<div className="container-fluid">
-	<div className="row">
-		<div className="col-md-12">
-			<h2 className="text-center">
-				Генерируем случайные страницы!
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2 className="text-center">
+                            Генерируем случайные страницы!
 			</h2>
-            {list}
-			<div className="row">
-				<div className="col-md-12">
-                    <button type="button" className="btn btn-secondary" onClick={() => this.handleRefresh()}>Сгенерировать ещё!</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                        {list}
+                        <div className="row">
+                            <div className="col-md-12">
+                                <button type="button" className="btn btn-secondary" onClick={() => this.handleRefresh()}>Сгенерировать ещё!</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
-export default WikiApp; 
+export default WikiApp;
+export { titler };
