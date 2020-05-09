a403695f8970000cd51523406cddd1abe9c87120
Lev Kuzmin <liokuza@gmail.com>
2020-05-09 19:03:34 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ import './index.css';
 import WikiApp from './wikiapp';
 import * as serviceWorker from './serviceWorker';
 
-const express = require("express");
-
-const app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <WikiApp />
@@ -23,8 +14,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
