9a006dca52968f7ea1303db6ffb3a908b9ad9fe5
Lev Kuzmin <liokuza@gmail.com>
2020-05-10 17:48:36 +0300

    here we go again 11

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const path = require('path');
 
 const port = process.env.PORT || 8080;
 const app = express();
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
+
 app.use(favicon(`${__dirname}/build/favicon.ico`));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
